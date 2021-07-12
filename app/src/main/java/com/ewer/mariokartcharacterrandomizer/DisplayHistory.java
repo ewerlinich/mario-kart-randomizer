b4d571b44153e8adb5fff210afccd0b93944d9b3
Ezra Werlinich <ewerlinich@gmail.com>
2021-07-12 13:36:06 -0400

    Created new activities for displaying generated RandomBuild history and saved builds

---
+++ app/src/main/java/com/ewer/mariokartcharacterrandomizer/DisplayHistory.java
@@ -0,0 +1,13 @@
+package com.ewer.mariokartcharacterrandomizer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayHistory extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+}
---
+++ app/src/main/java/com/ewer/mariokartcharacterrandomizer/DisplaySavedBuilds.java
@@ -0,0 +1,13 @@
+package com.ewer.mariokartcharacterrandomizer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplaySavedBuilds extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saved_builds);
+    }
+}
